2c7aca91b71d2d029a4f51fd16a6b7b15df90331
ShounaKnk <shounakkannaik0607@gmail.com>
2025-06-11 02:27:13 +0530

    system audio gets recorded and normalized

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('we came in')
+import soundcard as sc
+import soundfile as sf
+import numpy as np
+
+SAMPLE_RATE = 48000
+DURATION = 5
+
+speaker = sc.default_speaker()
+
+mic = sc.get_microphone(speaker.name, include_loopback=True)
+
+with mic.recorder(samplerate=SAMPLE_RATE) as recorder:
+    audio_data = recorder.record(numframes=SAMPLE_RATE*DURATION)
+
+def normalize_audio(audio_file):
+    max_val = np.max(np.abs(audio_file))
+    if max_val >0:
+        audio_file /= max_val
+        audio_file *= 0.9
+    return audio_file
+
+audio_data = normalize_audio(audio_data)
+
+sf.write('test.wav', audio_data, SAMPLE_RATE)
+print('saved')
