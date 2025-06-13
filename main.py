e064cd89dbc09b10ed0303fd38d6c0f96d7d3caf
ShounaKnk <shounakkannaik0607@gmail.com>
2025-06-13 20:36:19 +0530

    audio is streaming but crashes

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import soundcard as sc
 import soundfile as sf
 import numpy as np
+import socket
+import threading
+import time
 
 SAMPLE_RATE = 48000
-DURATION = 5
+CHANNEL = 2
+CHUNK_SIZE = 1024
+PORT = 50007
 
 speaker = sc.default_speaker()
-
 mic = sc.get_microphone(speaker.name, include_loopback=True)
 
-with mic.recorder(samplerate=SAMPLE_RATE) as recorder:
-    audio_data = recorder.record(numframes=SAMPLE_RATE*DURATION)
+# Socket Setup
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', PORT))
+server_socket.listen(1)
+
+print(f"waiting for the client on port {PORT}...")
+
+client_socket, addr = server_socket.accept()
+print(f'client connected from {addr}')
 
 def normalize_audio(audio_file):
     max_val = np.max(np.abs(audio_file))
@@ -19,7 +30,20 @@ def normalize_audio(audio_file):
         audio_file *= 0.9
     return audio_file
 
-audio_data = normalize_audio(audio_data)
-
-sf.write('test.wav', audio_data, SAMPLE_RATE)
-print('saved')
+with mic.recorder(samplerate=SAMPLE_RATE) as recorder:
+    print('streaming started')
+    try:
+        while True:
+            time.sleep(0.01)
+            data = recorder.record(numframes=CHUNK_SIZE)
+            data = normalize_audio((data))
+            pcm = (data*32767).astype(np.int16).tobytes()
+            print(f"Sending chunk: {len(pcm)} bytes")
+            client_socket.sendall(pcm)
+    except BrokenPipeError:
+        print('Client Disconnected')
+    except KeyboardInterrupt:
+        print('server stopped')
+    
+client_socket.close()
+server_socket.close()
